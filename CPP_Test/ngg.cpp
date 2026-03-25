9c8195477042450a940e0e1d255df0fcb18affe0
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-03-25 21:07:00 +0600

    Initial Commit

---
+++ CPP_Test/ngg.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+void display();
+void gameloop(int num, int guess, int tries);
+int main()
+{
+    int num, guess, tries = 0;
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+    
+    display();
+    
+    gameloop(num, guess, tries);
+    
+    return 0;
+}
+
+void display() {
+    std::cout << "\n+--------------------------------------+";
+    std::cout << "\n|>>>      NUMBER GUESSING GAME      <<<|";
+    std::cout << "\n+--------------------------------------+";
+    std::cout << std::endl;
+}
+void gameloop(int num, int guess, int tries) {
+    do
+    {
+        std::cout << "\nEnter number guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+
+        if (guess > num) std::cout << '\n' << "Guess is too high.";
+        else if (guess < num) std::cout << '\n' << "Guess is too low..";
+        else
+        {
+            std::cout << "\n\n" << "CORRECT GUESS!!!";
+            std::cout << '\n' << "You tried " << tries << " times.";
+        }
+        
+    } while (guess != num);
+}
