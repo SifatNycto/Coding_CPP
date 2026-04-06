8efe60b89f849e1ca485206da9e4c0f0cd739f22
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-04-06 22:21:23 +0600

    fdfdsff

--- OOP/cub/random_question.cpp
+++ OOP/cub/random_question.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+// class Dog {
+//     private:
+//         std::string tailType;
+//         std::string colorOfBody;
+//         int runCapability;
+//     public:
+//         Dog() {
+//             tailType = "straight";
+//             colorOfBody = "black";
+//         }
 
-    return 0;
-}
+//         void Run() {
+//             std::cout << "The Dog is running" << std::endl;
+//         }
+
+//         std::string getTailType() {
+//             return tailType;
+//         }
+
+// };
+
+// int main()
+// {
+//     Dog d;
+    
+//     d.Run();
+    
+//     return 0;
+// }
+
+
+
+
+
+// class Employee {
+//     private:
+//         int salary;
+//     public:
+//         Employee() : salary(5000) {}
+
+//         int setSalary() {
+//             return salary;
+//         }
+
+// };
+
+// int main()
+// {
+//     Employee e;
+
+//     std::cout << e.setSalary();
+//     return 0;
+// }
+
+
+
+
+
+
--- Random_CPP/Random_1.cpp
+++ Random_CPP/Random_1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::string s;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, s);
+
+    std::cout << "Your entered: " << s << std::endl;
+
+    s.push_back('X');
+
+    std::cout << "After push back: " << s << std::endl;
+
+
+    while (s.size() != 0)
+    {
+        s.pop_back();
+
+        std::cout << "After pop back: " << s << std::endl;
+    }
 
     return 0;
 }
