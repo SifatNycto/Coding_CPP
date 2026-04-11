27f1d5dab28bbfd391ba0cbe0d7da0e51b556c61
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-04-11 20:09:10 +0600

    oop polymorphism

--- OOP/Learning/5_Polymorphism.cpp
+++ OOP/Learning/5_Polymorphism.cpp
@@ -15,27 +15,48 @@ they are used.
 // 1. Compile Time Polymorphism...............................
 // a) Constructor Overloading is an example of Compile Time Polymorphism
 
-class Student {
-    public:
-        std::string name;
+// class Student {
+//     public:
+//         std::string name;
 
-        Student() {
-            std::cout << "Non-parameterized\n";
-        }
+//         Student() {
+//             std::cout << "Non-parameterized\n";
+//         }
         
-        Student(std::string name) {
-            this->name = name;
-            std::cout << "Parameterized\n";
-        }
-};
-int main()
-{
-    //Student s1;
-    Student s1("Tony Stark");
+//         Student(std::string name) {
+//             this->name = name;
+//             std::cout << "Parameterized\n";
+//         }
+// };
+// int main()
+// {
+//     //Student s1;
+//     Student s1("Tony Stark");
 
-    return 0;
-}
+//     return 0;
+// }
+
+//>>>>>>>>>>>>>>>>>>>>>>>
 
 // b) Function or Method Overloading 
 // example or implementation of Compile Time Polymorphism
 
+class Print {
+    public:
+        void show(int x) {
+            std::cout << "int: " << x << std::endl;
+        }
+
+        void show(char ch) {
+            std::cout << "char: " << ch << std::endl;
+        }
+};
+
+int main()
+{
+    Print p1;
+    p1.show(354);
+    p1.show('&');
+
+    return 0;
+}
