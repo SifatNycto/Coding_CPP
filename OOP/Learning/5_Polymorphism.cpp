868c29fbef09ce621cb5aa9ed143d20d54c10436
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-04-12 22:24:30 +0600

    fhsdad

--- OOP/Learning/5_Polymorphism.cpp
+++ OOP/Learning/5_Polymorphism.cpp
@@ -88,10 +88,63 @@ they are used.
     Overriding is dependent upon Inheritance
 */
 
+class Parent {
+    public:
+        void Info() {
+            std::cout << "Child Classes\n";
+        }
+
+        void getInfo() {
+            std::cout << "Parent Class\n";
+        }
+};
+
+class Child : public Parent {
+    public:
+        void Info() {
+            std::cout << "Child Classes\n";
+        }
+        void getInfo() {
+            std::cout << "Child Class\n";
+        }
+};
+
+int main()
+{
+    Child c1;
+    c1.getInfo();
+    //c1.Info();
+
+    Parent p1;
+    p1.Info();
+
+
+    return 0;
+}
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 2.2) Virtual Functions.......................... Similar to Overriding
+/* A Virtual function is a member function that you expect to be redefined in derived classes. */
+/*
+    Virtual Functions are Dynamic in nature.
+
+    Defined by the keyword "virtual" inside a base class and are always declared 
+    with a base class and overridden in a child class.
+
+    A virtual function is called during Runtime.............
+*/
+
 // class Parent {
 //     public:
 //         void getInfo() {
 //             std::cout << "Parent Class\n";
+//         }
+
+//         // Defined as 'virtual' inside Base (Parent) Class
+//         virtual void hello() {
+//             std::cout << "Hello from Parent\n";
 //         }
 // };
 
@@ -100,54 +153,20 @@ they are used.
 //         void Info() {
 //             std::cout << "Child Class\n";
 //         }
+
+//         // Re-implement.......... (Overridden in Child Class)
+//         void hello() {
+//             std::cout << "Hello from Child\n";
+//         }
 // };
 
 // int main()
 // {
-//     // Child c1;
-//     // c1.getInfo();
-//     // c1.Info();
+//     Child c1;
+//     c1.hello();
 
 //     Parent p1;
-//     p1.getInfo();
+//     p1.hello();
+
 //     return 0;
 // }
-
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// 2.2) Virtual Functions.......................... Similar to Overriding
-/* A Virtual function is a member function that you expect to be redefined in derived classes. */
-// keyword is 'virtual'  //
-
-class Parent {
-    public:
-        void getInfo() {
-            std::cout << "Parent Class\n";
-        }
-
-        virtual void hello() {
-            std::cout << "Hello from Parent\n";
-        }
-};
-
-class Child : public Parent {
-    public:
-        void Info() {
-            std::cout << "Child Class\n";
-        }
-
-        // Re-implement...............
-        void hello() {
-            std::cout << "Hello from Child\n";
-        }
-};
-
-int main()
-{
-    Child c1;
-    c1.hello();
-
-    
-    return 0;
-}
