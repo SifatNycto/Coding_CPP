9ee1eb4eac0b47299a36aad69626d2d9ee7e9ba0
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-04-23 09:14:57 +0600

    Added Comments in code of 4pillars in one shot

--- OOP/Learning/4Pillars.cpp
+++ OOP/Learning/4Pillars.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 #include <string>
 
+// Base class (Abstraction + Encapsulation)
 class Shape {
     protected:
+        // Encapsulation (data hidden inside class)    
         std::string name;
+
     public:
+        // Constructor
         Shape(std::string n) : name(n) {}
 
+        // Pure virtual function (Abstraction)
         virtual void draw() = 0;
 
+        // Normal function || method
         void showName() {
             std::cout << "Shape: " << name << std::endl;
         }
 };
 
+// Derived class (Inheritance)
 class Circle : public Shape {
     public:
         Circle() : Shape("Circle") {}
 
+        // Overriding (Polymorphism)
         void draw() {
             std::cout << "Drawing Circle" << std::endl;
         }
@@ -26,9 +34,14 @@ class Circle : public Shape {
 int main()
 {
     Circle c;
+
+    // Reference (used for polymorphism)
     Shape& s = c;
 
+    // Encapsulation access
     s.showName();
+
+    // Runtime polymorphism
     s.draw();
 
     return 0;
