f5dcd9de133bd01661cf5b7137c14a69cd170870
SifatNycto <sftmhd@gmail.com>
2026-07-18 03:19:14 +0600

    Add partial part of stack: infix to postfix code

--- CPP_DSA/infix_to_postfix.cpp
+++ CPP_DSA/infix_to_postfix.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-// #include <stack>
+#include <stack>
 
 // For Alphabets
 
@@ -15,47 +15,156 @@
         2. highest priority operator will not stay when lowest will come, highest will 
         3. if ')' found then pop all operators between '(' and ')'
     */
-int main()
-{
-    std::string init_stack;
-    std::string operator_stack;
-    std::string pf_stack;
 
-    std::cin >> init_stack;
-
-    // A+B/C*D-E/(F+G)  initial
-    // ABC/D*+EFG+/-    final
-
-    char op = '(';
-    char cp = ')';
+namespace opM {
+    char op  = '(';
+    char cp  = ')';
     char exp = '^';     // 3
     char ast = '*';     // 2
     char div = '/';     // 2
     char mod = '%';     // 2
     char sum = '+';     // 1
     char sub = '-';
+}
 
+namespace opV {
+    int op  = 4;
+    int cp  = 4;
+    int exp = 3;
+    int ast = 2;
+    int div = 2;
+    int mod = 2;
+    int sum = 1;
+    int sub = 1;
+}
+
+int main()
+{
+    std::string init_stack;
+    std::string operator_stack;
+    std::string postfix_stack;
+
+    std::cin >> init_stack;
+
+    // A+B/C*D-E/(F+G)  initial
+    // ABC/D*+EFG+/-    final
 
     
     for (int i = 0; i < init_stack.size(); i++)
     {
-        if (init_stack[i] != op && init_stack[i] != cp && init_stack[i] != exp && init_stack[i] != ast && init_stack[i] != div && init_stack[i] != mod && init_stack[i] != sum && init_stack[i] != sub)
+        if (init_stack[i] != opM::op && 
+            init_stack[i] != opM::cp && 
+            init_stack[i] != opM::exp && 
+            init_stack[i] != opM::ast && 
+            init_stack[i] != opM::div && 
+            init_stack[i] != opM::mod && 
+            init_stack[i] != opM::sum && 
+            init_stack[i] != opM::sub)
         {
-            pf_stack.push_back(init_stack[i]);
+            postfix_stack.push_back(init_stack[i]);
         }
         else 
         {
             operator_stack.push_back(init_stack[i]);
+            for (int j = operator_stack.size() - 1; j >= 0; j--)
+            {   
+                // rule 3
+                // (......) ')'
+                if (operator_stack[j] == opM::cp)
+                    for (int k = operator_stack.size() - 2; k >= 0; k--)
+                    {
+                        while (!operator_stack[opM::op])
+                            postfix_stack.push_back(operator_stack[k]);
+                    }
+                // rule 1
+                // ^ ^
+                if (operator_stack[j] == opM::exp && operator_stack[j-1] == opM::exp)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // * *
+                else if (operator_stack[j] == opM::ast && operator_stack[j-1] == opM::ast)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // * /
+                else if (operator_stack[j-1] == opM::ast && operator_stack[j] == opM::div)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // * %
+                else if (operator_stack[j-1] == opM::ast && operator_stack[j] == opM::mod)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();    
+                // / *
+                else if (operator_stack[j-1] == opM::div && operator_stack[j] == opM::ast)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // / %
+                else if (operator_stack[j-1] == opM::div && operator_stack[j] == opM::mod)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // / /
+                else if (operator_stack[j-1] == opM::div && operator_stack[j] == opM::div)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // % *
+                else if (operator_stack[j-1] == opM::mod && operator_stack[j] == opM::ast)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // % /
+                else if (operator_stack[j-1] == opM::mod && operator_stack[j] == opM::div)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // % %
+                else if (operator_stack[j-1] == opM::mod && operator_stack[j] == opM::mod)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // + +
+                else if (operator_stack[j-1] == opM::sum && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // + -
+                else if (operator_stack[j-1] == opM::sum && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // - -
+                else if (operator_stack[j-1] == opM::sub && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                // - +
+                else if (operator_stack[j-1] == opM::sub && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j]), operator_stack.pop_back();
+                
 
-            for (int j = 0; j < operator_stack.size(); j++)
-            {
+                // rule 2
+                // ^ *
+                else if (operator_stack[j-1] == opM::exp && operator_stack[j] == opM::ast)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // ^ /
+                else if (operator_stack[j-1] == opM::exp && operator_stack[j] == opM::div)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // ^ %
+                else if (operator_stack[j-1] == opM::exp && operator_stack[j] == opM::mod)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // ^ +
+                else if (operator_stack[j-1] == opM::exp && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // ^ -
+                else if (operator_stack[j-1] == opM::exp && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // * +
+                else if (operator_stack[j-1] == opM::ast && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // * -
+                else if (operator_stack[j-1] == opM::ast && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // / +
+                else if (operator_stack[j-1] == opM::div && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // / -
+                else if (operator_stack[j-1] == opM::div && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // % +
+                else if (operator_stack[j-1] == opM::mod && operator_stack[j] == opM::sum)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                // % -
+                else if (operator_stack[j-1] == opM::mod && operator_stack[j] == opM::sub)
+                    postfix_stack.push_back(operator_stack[j-1]), operator_stack.pop_back();
+                
+
 
             }
+            
         }
     }
 
 
-    std::cout << pf_stack;
+    std::cout << postfix_stack;
     std::cout << operator_stack;
 
 
--- Random_CPP/Random_1.cpp
+++ Random_CPP/Random_1.cpp
@@ -198,3 +198,215 @@
 
 //     return 0;
 // }
+
+
+
+
+
+
+// // intput A-B+B+A-A+A+B
+
+
+// int main()
+// {
+//     std::string word;
+//     std::cin >> word;
+
+//     int plus = 0;
+//     int minus = 0;
+
+//     for (int i = 0; i < word.size(); i++)
+//     {
+//         if (word[i] == '+') plus++;
+//         if (word[i] == '-') minus++;
+//     }
+
+//     std::cout << std::endl << "number of a: " << plus;
+
+//     std::cout << std::endl << "number of b: " << minus;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+// intput A-B+B+A-A+A+B
+
+// int main()
+// {
+//     std::string word;
+//     std::cin >> word;
+
+//     int op = 0;
+
+//     for (int i = 0; i < word.size(); i++)
+//     {
+//         if (word[i] == '+' || word[i] == '-') op++;
+//     }
+
+//     std::cout << std::endl << "number of op: " << op;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// ABABAAABBAAZBBZ 
+
+// int main()
+// {
+//     std::string word;
+
+//     std::cin >> word;
+
+//     int s = word.size();
+
+    
+    
+
+//     for (int i = 0; i < s; i++)
+//     {
+//         for (int j = 65; j < 91; j++)
+//         {
+//             int a = 0;
+            
+//             if (word[tolower(i)] == j) a++;
+
+//             std::cout << a;
+
+//             // else b++;
+//         }
+
+
+//     }
+
+//     std::cout << "a: " << std::endl << "b: ";
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// A+B+A-A+B-A
+
+// int main()
+// {
+//     std::string word;
+//     std::cin >> word;
+
+//     int plus = 0;
+//     int minus = 0;
+
+//     for (int i = 0; i < word.size(); i++)
+//     {
+//         if (word[i] == '+') plus++;
+//         if (word[i] == '-') minus++;
+
+        
+//     }
+
+//     std::cout << "plus: " << plus << std::endl << "minus: " << minus;
+
+
+    
+//     return 0;
+
+// }
+
+
+// abcdfffhjszdvnmj
+/*
+    for (int i = 0; i < size; i++)
+    {
+
+    }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+void ans();
+int main()
+{   int i, j, k, l;
+    std::cout <<  "Hey User"  << std:: endl<< std:: endl;
+    
+    std::cout <<  "  IF YOU WANT " << std:: endl ;
+    std::cout <<  "  Ans so input 1 or ask so input 2" << std:: endl;     
+    std::cout <<  std:: endl;
+    std::cout <<  "Selet one :"; 
+    std::cin  >>  i;
+    std::cout <<  std:: endl;
+    
+    if( i == 1) {
+        std:: cout << "Your input is 1"<<std:: endl;
+       ans();
+    }
+    
+    else if (i==2){
+        std:: cout << "Your input is 2"<<std:: endl;
+    }
+    
+    else 
+        std:: cout << "Your input is invalid"<<std:: endl;
+    
+
+    return 0;
+}
+
+
+void ans(){
+    int s;
+    std::string t;
+    std::string Q[5]={"how are you?", 
+    "what is your depertment?",
+    "What is your student id?",
+    "What is your name?",
+    "what class do you read in?"};
+    
+    for(int p=0;p<=5;p++){
+       std::cout <<  Q[p]<<std:: endl;
+       std::cout<<std::endl;
+       
+       std::cin.ignore ();
+       getline(std::cin, t );
+       
+       std::cout<<std::endl;
+    }
+    
+}
